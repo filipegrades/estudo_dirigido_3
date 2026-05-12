21329fc3c22ac502c141bfc6fd3ff2ffca6ddfa5
filipegrades <filipe.grades@estudante.ufjf.br>
2026-05-12 10:41:36 -0300

    Finalização da criação da senoide com ruído

--- main.c
+++ main.c
@@ -3,19 +3,21 @@
 #include "device.h"
 #include "board.h"
 #include "math.h"
-
+#include "stdlib.h"
 
 // Declaração dos defines
-#define N 100       // Tamanho do buffer
+#define N 16      // Tamanho do buffer
 #define PI 3.14     // Valor adotado para pi
 
 // Delaração das variáveis globais
 unsigned int g_sineMean = 2048;
 unsigned int g_sinePeak = 1000;
-unsigned int g_signalNoise = 50;
+int g_signalNoiseMax = 50;
+int g_signalNoiseMin = -50;
 static unsigned int g_iS = 0;
 float g_inputSignal;
 float g_inputSignalBuffer[N];
+float g_noise;
 
 // Main
 void main(void)
@@ -52,7 +54,8 @@ void main(void)
 // Função de interrupção do Timer de geração da senoide
 __interrupt void INT_sineTimer_ISR(void)
 {
-    g_inputSignal = g_sinePeak * sin(2*PI*g_iS/N) + g_signalNoise*rand();
+    g_inputSignal = g_sineMean + g_sinePeak * sin(2*PI*g_iS/N) + (rand() % (g_signalNoiseMax - g_signalNoiseMin + 1)) + g_signalNoiseMin;
+    //g_inputSignal = g_sineMean + g_sinePeak * sin(2*PI*g_iS/N);
     g_inputSignalBuffer[g_iS] = g_inputSignal;
     g_iS = (g_iS+1)%N;
 }
